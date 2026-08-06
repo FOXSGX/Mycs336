fb65c8af7bf85e3508ab98b6acdebb71c1f55ecc
Huzh-Windows <2371159163@qq.com>
2026-08-06 11:12:38 +0800

    finish class Embedding,class RMSNorm

--- official_assignment1/cs336_basics/model.py
+++ official_assignment1/cs336_basics/model.py
@@ -11,3 +11,30 @@ class Linear(torch.nn.Module):
 
     def forward(self,x):
         return x @ self.weight.T
+
+
+
+class Embedding(torch.nn.Module):
+    def __init__(self,num_embeddings,embedding_dim,device=None,dtype=None):
+        super().__init__()
+        self.weight = torch.empty((num_embeddings,embedding_dim),device=device,dtype=dtype)
+        self.weight = torch.nn.Parameter(self.weight)
+        torch.nn.init.trunc_normal_(self.weight,mean=0,std=1,a=-3,b=3)
+
+    def forward(self,token_ids):
+        return self.weight[token_ids]
+
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self,d_model,eps=1e-5,device=None,dtype=None):
+        super().__init__()
+        self.eps = eps
+        self.weight = torch.ones((d_model,),device=device,dtype=dtype)
+        self.weight = torch.nn.Parameter(self.weight)
+
+    def forward(self,x):
+        dtype = x.dtype
+        x = x.to(torch.float32)
+        z = torch.mean(x**2,dim=-1,keepdim=True)+self.eps
+        scale = torch.rsqrt(z)
+        return (x*scale*self.weight).to(dtype)
--- official_assignment1/tests/adapters.py
+++ official_assignment1/tests/adapters.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 from cs336_basics.tokenizer import Tokenizer
 from cs336_basics.train_bpe import train_bpe
 from cs336_basics.model import Linear
+from cs336_basics.model import Embedding
+from cs336_basics.model import RMSNorm
 
 import os
 from collections.abc import Iterable
@@ -54,8 +56,10 @@ def run_embedding(
     Returns:
         Float[Tensor, "... d_model"]: Batch of embeddings returned by your Embedding layer.
     """
+    embedding = Embedding(vocab_size,d_model)
+    embedding.load_state_dict({"weight":weights})
+    return embedding(token_ids)
 
-    raise NotImplementedError
 
 
 def run_swiglu(
@@ -382,8 +386,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
-
+    rmsnorm = RMSNorm(d_model,eps)
+    rmsnorm.load_state_dict({"weight":weights})
+    return rmsnorm(in_features)
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
     """Given a tensor of inputs, return the output of applying SiLU
